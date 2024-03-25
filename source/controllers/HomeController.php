b71a1b5b231482e842da3d893972c78448aa94be
martprenger <mart.prenger@gmail.com>
2024-03-25 17:21:20 +0100

    changed database server from mysql to sqlite

--- source/controllers/HomeController.php
+++ source/controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Source;
+namespace Source\controllers;
 
 class HomeController
 {
--- source/models/DatabaseModel.php
+++ source/models/DatabaseModel.php
@@ -2,42 +2,29 @@
 
 namespace Source\models;
 
-use PDO;
-use PDOException;
-use Dotenv\Dotenv;
+use SQLite3;
 
-class DatabaseModel
-{
-    private $conn;
+class DatabaseModel {
+    private $db;
 
-    public function __construct()
-    {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-        $dotenv->load();
-
-        $host = $_ENV['DB_HOST'];
-        $db = $_ENV['DB_DATABASE'];
-        $user = $_ENV['DB_USERNAME'];
-        $pass = $_ENV['DB_PASSWORD'];
-        $charset = 'utf8mb4';
-
-        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-        $options = [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES => false,
-        ];
-        try {
-            $this->conn = new PDO($dsn, $user, $pass, $options);
-        } catch (PDOException $e) {
-            throw new PDOException($e->getMessage(), (int)$e->getCode());
-        }
+    public function __construct() {
+        $this->db = new SQLite3('database/database.db');
+        $this->db->exec('CREATE TABLE IF NOT EXISTS user (name STRING, email STRING, password STRING)');
     }
 
-    public function getUsers()
-    {
-        $stmt = $this->conn->prepare('SELECT * FROM user');
-        $stmt->execute();
-        return $stmt->fetchAll();
+    public function getUsers() {
+        $results = $this->db->query('SELECT * FROM user');
+        $items = array();
+        while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+            $items[] = $row;
+        }
+        return $items;
+    }
+
+    public function __destruct() {
+        $this->db->close();
     }
 }
+
+
+

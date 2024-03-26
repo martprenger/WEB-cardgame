fd6edd749479d851ee9b0989fe0b7b94d238bd15
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-03-26 09:36:14 +0100

    nav bar made

--- view/layouts/nav.php
+++ view/layouts/nav.php
@@ -13,7 +13,7 @@ require_once 'view/layouts/header.php';
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="test">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Features</a>

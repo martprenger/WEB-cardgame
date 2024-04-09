84e83cc6b87a78c6d0fb64039757fc7ef64d09ec
martprenger <mart.prenger@gmail.com>
2024-04-09 09:26:25 +0200

    added cards

--- view/cards.php
+++ view/cards.php
@@ -3,8 +3,5 @@
 require_once 'view/layouts/nav.php';
 
 foreach ($cards as $card) {
-    echo "<div class='card'>";
-    echo "<h2>" . $card['card']->getName(). "</h2>";
-    echo "<p>Attack: " . $card['attributes'][0]->getPower() . "</p>";
-    echo "</div>";
+    require 'view/layouts/Card.php';
 }
--- view/layouts/Card.php
+++ view/layouts/Card.php
@@ -1,12 +1,4 @@
-<?php
-// Check if card data is provided
-if (!empty($card)) {
-    echo "<div class='card'>";
-    echo "<h2>" . $card['card']->getName(). "</h2>";
-    echo "<p>Attack: " . $card['attributes'][0]->getPower() . "</p>";
-    // Display other attributes as needed
-    echo "</div>";
-} else {
-    echo "<p>No card data available.</p>";
-}
-?>
+<div class='card'>
+    <h2> <?php echo $card['card']->getName() ?></h2>
+    <p> <?php echo $card['attributes'][0]->getPower() ?> </p>
+</div>

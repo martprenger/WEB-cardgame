8d1ad435e99a038035fbf37c9f85ef725b2968cd
shanos123 <60260010+shanos123@users.noreply.github.com>
2024-04-14 11:38:25 +0200

    create decks

--- view/decks.php
+++ view/decks.php
@@ -7,3 +7,9 @@ require_once 'view/layouts/nav.php';
 
 <h1>Decks</h1>
 <p>Welcome in my app.</p>
+
+<form action="create_deck.php" method="post">
+    <label for="deck_name">Deck Name:</label><br>
+    <input type="text" id="deck_name" name="deck_name"><br>
+    <input type="submit" value="Create Deck">
+</form>
